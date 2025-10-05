a81b842f055b0ae8cb057a24813ac1a1af80db6c
Braxton Hancock <braxdhancock@gmail.com>
2025-10-05 10:37:57 -0400

    home screen renevation

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,17 @@ export default {
       fontFamily: {
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
       },
+      colors: {
+        background: " #202124",
+        surface: "rgb(255 255 255 / 0.1)",
+        text: " #ffffff",
+        primary: "#2176ff",
+        secondary: "#33a1fd",
+        tertiary: "#fdca40",
+        warning: "#f79824",
+        error: "#dd1c1a",
+        success: "#0f9d58",
+      },
     },
   },
   plugins: [],
