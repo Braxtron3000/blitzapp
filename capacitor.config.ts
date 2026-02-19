6f91d665d847e73aa31306a27901b88f0e196d4d
Braxton Hancock <braxdhancock@gmail.com>
2026-02-19 15:27:13 -0500

    capacitor config localhost & lowercase milomode

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,11 +2,18 @@ import type { CapacitorConfig } from "@capacitor/cli";
 import { env } from "~/env";
 
 const config: CapacitorConfig = {
-  appId: "com.MiloMode.app",
-  appName: "MiloMode",
+  appId: "com.milomode.app",
+  appName: "milomode",
   webDir: ".next",
   server: {
-    url: env.NEXTAUTH_URL, //build wont change unless the actual ass    igned var changes not just the assigned var's value.
+    url: "https://localhost:3000", //env.NEXTAUTH_URL, //build wont change unless the actual ass    igned var changes not just the assigned var's value.
+  },
+  plugins: {
+    LocalNotifications: {
+      smallIcon: "ic_stat_icon_config_sample",
+      iconColor: "#488AFF",
+      sound: "beep.wav",
+    },
   },
 };
 
